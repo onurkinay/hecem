f2ac062eba2f79e7187b8d913c551aef400c39dd
onurkinay <teknoonur@outlook.com>
2016-03-23 19:49:17 +0200

    SesBot projesi eklendi

---
+++ Hecem/SesBot/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Net;
+namespace SesBot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        string line;
+        List<string> kelimeler = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button snd = sender as Button;
+            System.IO.StreamReader file = new System.IO.StreamReader( (snd.Tag.ToString() == "0") ? "kelime.txt" : "hece.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile("http://tts.voicetech.yandex.net/tts?format=mp3&quality=hi&platform=web&application=translate&lang=tr_TR&text="+line+"", "sesler/"+line+".mp3");
+                }
+            }
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
